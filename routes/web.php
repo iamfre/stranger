100d0bfec5c6f12d70d35649c0a212f91ac86dd2
iamfre <zycs@yandex.ru>
2021-12-02 23:16:24 +0500

    fix web routes

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,6 @@ Route::get('/contact', [PagesController::class, 'contact'])->name('contact');
 Route::post('/add-comment', [CommentController::class, 'store'])->name('comment.create');
 Route::post('/feedback', [FeedbackController::class, 'store'])->name('feedback.create');
 Route::post('/subscribe', [SubscribeController::class, 'store'])->name('subscribe.create');
-Route::get('/category/{id}', [ArticlesController::class, 'categoryShow'])->name('category.show');
-
-Route::name('articles.')->prefix('articles')->middleware('auth')->group(function () {
-    Route::get('/', [ArticlesController::class, 'index'])->name('index');
-    Route::get('/create', [ArticlesController::class, 'create'])->name('create');
-    Route::post('/create', [ArticlesController::class, 'store']);
-    Route::get('/search', [SearchController::class, 'search'])->name('search');
-    Route::get('/{article}/edit', [ArticlesController::class, 'edit'])->name('edit');
-    Route::post('/{article}/update', [ArticlesController::class, 'update'])->name('update');
-    Route::delete('/{article}/destroy', [ArticlesController::class, 'destroy'])->name('destroy');
-    Route::patch('/{id}/publish', [ArticlesController::class, 'publish'])->name('publish');
-    Route::get('/{article:slug}', [ArticlesController::class, 'show'])->name('show');
-});
 
 Route::name('user.')->group(function () {
     Route::get('/private', [PagesController::class, 'private'])->middleware('auth')->name('private');
@@ -38,3 +25,16 @@ Route::name('user.')->group(function () {
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 
+Route::name('articles.')->prefix('articles')->middleware('auth')->group(function () {
+    Route::get('/', [ArticlesController::class, 'index'])->name('index');
+    Route::get('/create', [ArticlesController::class, 'create'])->name('create');
+    Route::post('/create', [ArticlesController::class, 'store']);
+    Route::get('/search', [SearchController::class, 'search'])->name('search');
+    Route::get('/{article}/edit', [ArticlesController::class, 'edit'])->name('edit');
+    Route::post('/{article}/update', [ArticlesController::class, 'update'])->name('update');
+    Route::delete('/{article}/destroy', [ArticlesController::class, 'destroy'])->name('destroy');
+    Route::patch('/{id}/publish', [ArticlesController::class, 'publish'])->name('publish');
+});
+
+Route::get('/category/{id}', [ArticlesController::class, 'categoryShow'])->name('category.show');
+Route::get('/{article:slug}', [ArticlesController::class, 'show'])->name('articles.show');
